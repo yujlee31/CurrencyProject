7593a729df4f959fa4006e28a3df5bff7f4d190f
jinsang park <jsgd1254@gmail.com>
2024-11-22 17:18:28 +0900

    init

---
+++ src/main/java/com/sparta/currency_user/CurrencyUserApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.currency_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyUserApplication.class, args);
+    }
+
+}
